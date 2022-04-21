d57f9764d173fd019acebcb2c64c0a9790bc6fac
minuxx <minux8905@gmail.com>
2022-04-22 03:20:33 +0900

    Feat: Naver Map 적용

--- app/src/main/java/com/minux/mask_alarmi/presentation/map/MapFragment.kt
+++ app/src/main/java/com/minux/mask_alarmi/presentation/map/MapFragment.kt
@@ -1,16 +1,67 @@
 package com.minux.mask_alarmi.presentation.map
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
 import com.minux.mask_alarmi.R
 import com.minux.mask_alarmi.core.BaseFragment
 import com.minux.mask_alarmi.databinding.FragmentMapBinding
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
+class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnMapReadyCallback {
+    private lateinit var naverMap: NaverMap
+
     override fun initDataBinding() {
 
     }
 
     override fun initAfterBinding() {
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+
+        binding.mapView.getMapAsync(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
     }
 }
